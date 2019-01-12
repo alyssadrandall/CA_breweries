63fd6131b14ead94ebc7f91e7ac528bfdfaf27e1
Alyssa Kennedy <alyssadrandall@gmail.com>
2019-01-12 17:24:10 +0000

    updated environment

--- lib/CA_breweries.rb
+++ lib/CA_breweries.rb
@@ -2,8 +2,3 @@ require_relative "./CA_breweries/version"
 require_relative "./CA_breweries/API"
 require_relative "./CA_breweries/Brewery"
 require_relative "./CA_breweries/CLI"
-
-module CABreweries
-  class Error < StandardError; end
-  # Your code goes here...
-end
