d84d15641e1437c81d8d562ffebf2bdca035046b
Alyssa Kennedy <alyssadrandall@gmail.com>
2019-01-12 18:42:20 +0000

    Updated CLI

--- lib/CA_breweries/CLI.rb
+++ lib/CA_breweries/CLI.rb
@@ -4,6 +4,7 @@ class CABreweries::CLI
   def call 
     puts "Welcome to your directory of California breweries!"
     menu
+    goodbye
   end
   
   def menu
@@ -18,13 +19,27 @@ class CABreweries::CLI
         brweriews_by_city
       when "name"
         breweries_by_name
+      else 
+        puts "Not sure what you're requesting. To search for breweries by city, enter 'city', to search for breweries by name, enter 'name', or enter 'exit' to exit."
       end
     end
   end
   
   def breweries_by_city
-    puts "To see a list of breweries and their details, please enter the city you in which you wish to search:" 
-    # here is where we pull from the api the CA cities and their respective breweries + details 
+    puts "To see a list of breweries in a specific city, please enter the city's name you in which you wish to search, or enter 'back' to return to the main menu." 
+      input = nil 
+      input = gets.strip.downcase
+      if "*--city?--*"
+        #pull information on that city and its respective breweries
+        puts "To see details about a specific brewery, enter its name."
+          brewery_details
+      elsif "back"
+        menu
+      end 
+    end 
+  end
+      
+    
     # CABreweries::API.get_breweries
   end 
   
